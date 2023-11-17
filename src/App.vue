a630bb2965e67ba36b88081cce677233a8410435
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-17 18:00:26 +0100

    add little style

--- src/App.vue
+++ src/App.vue
@@ -109,8 +109,6 @@ export default {
   <main>
     <div class="container my-4">
       <div class="row">
-        <h1>numero pagine: {{ this.pages }}</h1>
-        <h1> pagina corrente: {{ this.currentPage }}</h1>
         <div class="col-4 g-4" v-for="project in  this.projects ">
           <div class="card h-100">
             <div class="card-header">
@@ -119,9 +117,9 @@ export default {
                 <span>{{ project.type.name }}</span>
               </div>
             </div>
-            <div class="card-body">
-              <img class="img-fluid" :src="this.baseURL + 'storage/placeholders/' + project.cover_image" alt="">
-              <p>{{ project.content }}</p>
+            <div class="card-body d-flex flex-column">
+              <img class="w-75 m-auto " :src="this.baseURL + 'storage/placeholders/' + project.cover_image" alt="">
+              <p class="text-truncate">{{ project.content }}</p>
               <div class="technologies d-flex justify-content-center">
                 <div class="mx-1 badge rounded-pill text-bg-primary" v-for=" technology  in  project.technologies ">
                   <p class="m-0">{{ technology.name }}</p>
@@ -137,12 +135,24 @@ export default {
         </div>
       </div>
 
-      <div class="pagination d-flex justify-content-between">
-        <div class="dot">
-          <button class="btn-primary" @click="prev()">prev</button>
+      <div class="pagination d-flex justify-content-between mt-3">
+        <div class="dot" @click="prev()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+            class="bi bi-arrow-left-square-fill text-danger" viewBox="0 0 16 16">
+            <path class="bg-danger"
+              d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z" />
+          </svg>
         </div>
-        <div class="dot">
-          <button class="btn-primary" @click="next()">next</button>
+        <div class="current">
+          <h4> page: {{ this.currentPage }}</h4>
+
+        </div>
+        <div class="dot " @click="next()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+            class="bi bi-arrow-right-square-fill text-danger" viewBox="0 0 16 16">
+            <path class="bg-danger"
+              d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z" />
+          </svg>
         </div>
       </div>
 
